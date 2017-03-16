db776dde29f06fb0bffc6e9606baf4d514952786
SungHo.You <hahaysh@hotmail.com>
2017-03-16 22:53:23 +0900

    New projects

---
+++ FormsApp01/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FormsApp01
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
